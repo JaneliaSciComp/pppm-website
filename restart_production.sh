dc15ce01a0021f4778a09b617747e0afc7cf07cc
Robert R Svirskas <svirskasr@vmdmz128.int.janelia.org>
2023-03-02 16:55:41 -0500

    Configuration for new production host

--- restart_production.sh
+++ restart_production.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sudo /usr/local/bin/docker-compose down
-sudo docker image ls | grep registry.int.janelia.org | awk '{print $3}' | xargs docker image rm
+sudo docker-compose down
+sudo docker image ls | grep pppm | awk '{print $3}' | xargs sudo docker image rm
 sudo docker volume rm pppm-website_static_volume
-sudo docker pull registry.int.janelia.org/janeliascicomp/pppm-website
-sudo /usr/local/bin/docker-compose up -d
+sudo docker-compose -f docker-compose-prod.yml up -d
